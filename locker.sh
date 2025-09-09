1e1e163646a10e62c4acad5106aa68d6c386e1bb
Ant <marxguey@proton.me>
2025-09-09 15:11:46 -0700

    Sync to github (please)

---
+++ locker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/ksh
+i3lock -c 22222233 -e --indicator --ring-color=491510 --ring-width 12 --no-modkey-text
---
+++ powermenu.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/ksh
+function areyousure {
+	options="no\nyes"
+	yesno=$(echo -e "$options" | dmenu -l 6 -c -p 'Are you sure?')
+	if [[ $yesno = "yes" ]]; then
+		$yes
+	elif [[ $yesno = "no" ]]; then
+		exit
+	fi
+}
+
+
+
+
+if [ "$SESSION_TYPE" = "X11" ];
+then
+function powermenu {
+	options="cancel\nlock\nsleep\nshutdown\nrestart\nexit dwm\nexit openbox"
+	selected=$(echo -e "$options" | dmenu -l 6 -c -p hi)
+	if [[ $selected = "shutdown" ]]; then
+#		yes="doas openrc-shutdown -p now"
+		yes="loginctl poweroff"
+		areyousure
+	elif [[ $selected = "restart" ]]; then
+#		yes="doas openrc-shutdown -r now"
+		yes="loginctl reboot"
+		areyousure
+	elif [[ $selected = "sleep" ]]; then
+#		loginctl lock-session && sleep 1;doas s2ram
+		loginctl suspend
+	elif [[ $selected = "lock" ]]; then
+		loginctl lock-session
+	elif [[ $selected = "cancel" ]]; then
+		return
+	elif [[ $selected = "exit dwm" ]]; then
+		pkill dwm && pkill xinit
+	elif [[ $selected = "exit openbox" ]]; then
+		openbox --exit
+	fi
+}
+powermenu
+
+
+
+
+
+elif [ "$SESSION_TYPE" = "Wayland" ]
+then
+function powermenu {
+	options="cancel\nlock\nsleep\nshutdown\nrestart\nexit sway"
+	selected=$(echo -e "$options" | dmenu -l 6 -c)
+	if [[ $selected = "shutdown" ]]; then
+#		yes="doas openrc-shutdown -p now"
+		yes="loginctl poweroff"
+		areyousure
+	elif [[ $selected = "restart" ]]; then
+#		yes="doas openrc-shutdown -r now"
+		yes="loginctl reboot"
+		areyousure
+	elif [[ $selected = "sleep" ]]; then
+		swaylock & sleep 1;doas s2ram
+	elif [[ $selected = "lock" ]]; then
+		swaylock
+	elif [[ $selected = "cancel" ]]; then
+		return
+	elif [[ $selected = "exit sway" ]]; then
+		swaymsg kill
+	fi
+}
+powermenu
+
+
+fi
