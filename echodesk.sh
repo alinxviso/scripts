430cae0406ac5a0e83f891ad4290c5af7db750df
Ant <marxguey@proton.me>
2025-09-09 15:26:36 -0700

    Move echodesk from /home to /run to reduce disk wear

--- echodesk.sh
+++ echodesk.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-wmctrl -d | awk '/\*/{print $NF}' | head -c 1 | tee /run/user/`id -u`/desktopchange.log; echo
+wmctrl -d | awk '/\*/{print $NF}' | head -c 1 | tee ~/.scripts/desktopchange.log; echo
