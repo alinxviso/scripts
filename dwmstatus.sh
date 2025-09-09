9da04daf4b9002b60b87ac0f59d48c430ea60cfd
Ant <marxguey@proton.me>
2025-09-09 15:46:29 -0700

    Fix dwmstatus and add make faster via "EUID"

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/ksh
 
+# As to not run "id -u" every 0.1 seconds
+EUID=$(id -u)
+
 # Checks if the device is a laptop
 if [ -e /sys/class/power_supply/AC ]; then
 	while true
 	do
-	echo "$(sensors thinkpad-isa-0000 | awk '/CPU/{print ($2)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/"$(id -u)"/dwmstatus
+	echo "$(sensors thinkpad-isa-0000 | awk '/CPU/{print int($2)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
 	sleep 2
 	done &
 
 	while true
 	do
-	xsetroot -name "$(cat /run/user/$(id -u)/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +'%Y/%m/%d/%a/%H:%M:%S')"
+	xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +"%Y/%m/%d/%a/%H:%M:%S")"
 	sleep 0.1
 	done &
 else	
 # If it's not, it's a gigabyte pc
 	while true
 	do
-	echo "$(sensors gigabyte_wmi-virtual-0 | awk '/temp4/{print ($2)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/"$(id -u)"/dwmstatus
+	echo "$(sensors gigabyte_wmi-virtual-0 | awk '/temp4/{print int($2)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
 	sleep 2
 	done &
 	
 	while true
 	do
-	xsetroot -name "$(cat /run/user/$(id -u)/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +'%Y/%m/%d/%a/%H:%M:%S')"
+	xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +"%Y/%m/%d/%a/%H:%M:%S")"
 	sleep 0.1
 	done &
 fi
