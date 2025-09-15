8846ddbf93883b5850cb0ea45ff7410d461f1f39
Ant <marxguey@proton.me>
2025-09-15 12:56:06 -0700

    fix dzen2-justworks

--- vermaden/dzen2-justworks.sh
+++ vermaden/dzen2-justworks.sh
@@ -7,15 +7,13 @@ DELAY=60
 FONT='Ubuntu Mono-10'
 FG='#eeeeee'
 BG='#222222'
-NR="(sh|tail)\ *dzen2-fifo"
-ME=$$
 DOS='onstart=raise'
 #DOS='onstart=lower'
-DB1='button1=exec:~/.scripts/vermaden/dzen2-update.sh > /run/user/"$(id -u)"/dzen2-fifo'
-DB2='button2=exec:~/.scripts/vermaden/xdotool.sh workmenu'
-DB3='button3=exec:~/.scripts/vermaden/xdotool.sh menu'
-DB4='button4=exec:~/.scripts/vermaden/xdotool.sh down'
-DB5='button5=exec:~/.scripts/vermaden/xdotool.sh up'
+#DB1='button1=exec:~/.scripts/vermaden/dzen2-update.sh > /run/user/"$(id -u)"/dzen2-fifo'
+DB1='button2=exec:~/.scripts/vermaden/xdotool.sh workmenu'
+DB2='button3=exec:~/.scripts/vermaden/xdotool.sh menu'
+DB3='button4=exec:~/.scripts/vermaden/xdotool.sh down'
+DB4='button5=exec:~/.scripts/vermaden/xdotool.sh up'
 
 # first we kill dzen2, assuming you haven't changed the binary name or smth
 killall dzen2
@@ -30,12 +28,12 @@ do
 	~/.scripts/vermaden/dzen2-update.sh
 	sleep 1
 done | dzen2 \
-	-w 3600
-	-fg "${FG}"
-	-bg "${BG}"
-	-fn "${FONT}"
-	-ta l
-	-e "${DOS};${DB1};${DB2};${DB3};${DB4};${DB5}" &
+	-w 3600 \
+	-fg "${FG}" \
+	-bg "${BG}" \
+	-fn "${FONT}" \
+	-ta l \
+	-e "${DOS};${DB1};${DB2};${DB3};${DB4}" &
 
 # you can change "-ta l" to "-ta c" or "-ta r" for centered and right respectively
 
