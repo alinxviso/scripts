b91eb663fce3ce4995410e894a3ddefd9402c217
Ant <marxguey@proton.me>
2025-10-30 14:22:46 -0700

    tidy up and add brightness for laptop

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -14,14 +14,14 @@ if [ -e /sys/class/power_supply/AC ]; then
 
 	while true
 	do
-	xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) VOL:$(pamixer --get-volume-human) | $(date +"%Y/%m/%d/%a/%I:%M:%S%P")"
+		xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) bright:$(brightnessctl -m | cut -d ',' -f 4) | vol:$(pamixer --get-volume-human) | $(date +"%Y/%m/%d/%a/%I:%M:%S%P")"
 	sleep 0.1
 	done &
 else	
-# If it's not, it's a gigabyte pc
+# If it's not, don't show backlight or battery
 	while true
 	do
-	echo "$(sensors gigabyte_wmi-virtual-0 | awk '/temp3/{print int($2)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
+	echo "$(sensors coretemp-isa-0000 | awk '/P/{print int($4)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
 	sleep 2
 	done &
 	
