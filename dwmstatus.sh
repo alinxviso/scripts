1dc7402bec822d197e7b57def02946ee200668f9
Ant <marxguey@proton.me>
2025-11-10 19:27:03 -0800

    add euid for shells other than bash

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# As to not type 'id -u' a million times, only use if /bin/sh is not a symlink to bash
-# EUID=$(id -u)
+# to support euid if /bin/sh isn't bash
+sh="$(realpath /bin/sh)"
+truncsh="${sh##*/}"
+if [ "$truncsh"  != "bash" ]; then
+	EUID=$(id -u)
+fi
 
 # Checks if the device is a laptop
 if [ -e /sys/class/power_supply/AC ]; then
@@ -21,12 +25,14 @@ else
 # If it's not, don't show backlight or battery
 	while true
 	do
-	echo "$(sensors coretemp-isa-0000 | awk '/P/{print int($4)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
+#	echo "$(sensors coretemp-isa-0000 | awk '/P/{print int($4)} ')°C MEM:$(free --mega | awk '/^Mem:/{print int($3)}')mb/$(free --mega | awk '/^Mem:/{print int($2)}')mb" > /run/user/${EUID}/dwmstatus
+	echo "$(~/.scripts/statusbar/sys) |" > /run/user/${EUID}/dwmstatus
 	sleep 2
 	done &
 	
 	while true
 	do
+#	xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +"%Y/%m/%d/%a/%I:%M:%S%P")"
 	xsetroot -name "$(cat /run/user/${EUID}/dwmstatus) VOL:$(pamixer --get-volume-human) $(date +"%Y/%m/%d/%a/%I:%M:%S%P")"
 	sleep 0.1
 	done &
